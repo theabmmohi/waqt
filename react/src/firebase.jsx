3a69c41372fe6b22b714002326bec5872bf16ea0
theabmmohi <theabmmohi@gmail.com>
2026-07-10 09:36:52 +0600

    Init FCM Imp

---
+++ react/src/firebase.jsx
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app"
+const VAPID = "BMMcgYgujvVNnheTOZ_TN2hYWGwcu6JefeibwNbX9KzkyzzI4k_vCMb5RtL4LgF9vvrTBQQT1dX3PqBFunLITR0"
+export default initializeApp({
+  appId: "1:696930371666:web:42887d9261a17671ca378b",
+  apiKey: "AIzaSyDbp0UHNrX4mp5Z6SMr81sGQwjYrFIgNeA",
+  storageBucket: "waqt-fcm.firebasestorage.app",
+  authDomain: "waqt-fcm.firebaseapp.com",
+  messagingSenderId: "696930371666",
+  projectId: "waqt-fcm"
+})
--- react/src/supabase.jsx
+++ react/src/supabase.jsx
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js"
+
 export default createClient(
   "https://khncuismpxnrbztqevcb.supabase.co",
   "sb_publishable_p9hGuErj9-j4x9rigJaCMg_fM-OCE1G",
-  {
-    auth: {
+  { auth: {
       experimental: { passkey: true },
       flowType: "pkce"
-    }
-  }
+  } }
 )
