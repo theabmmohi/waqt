df81cdd82a889a5f18fb5205ad376d5b62b07832
theabmmohi <theabmmohi@gmail.com>
2026-07-11 09:20:34 +0600

    Fixed Api Base For Native

--- react/src/api.jsx
+++ react/src/api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios"
+import { Capacitor } from "@capacitor/core"
 import Supabase from "@/supabase"
 
-const base = window.location.hostname.replace(/^[^.]+/, "api")
-const api = axios.create({
-  baseURL: `https://${base}`
-})
+const base = Capacitor.isNativePlatform() ? "api.abm.ami.bd" : window.location.hostname.replace(/^[^.]+/, "api")
+const api = axios.create({ baseURL: `https://${base}` })
+
 api.interceptors.request.use(async (config) => {
   const { data } = await Supabase.auth.getSession()
   const token = data?.session?.access_token
