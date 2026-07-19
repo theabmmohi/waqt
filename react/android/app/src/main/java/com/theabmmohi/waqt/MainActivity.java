8d14b60cf81b4d9ae34102f6dbb6aaa62fe28c43
theabmmohi <theabmmohi@gmail.com>
2026-07-19 15:48:00 +0600

    No Zoom In Native 😑

--- react/android/app/src/main/java/com/theabmmohi/waqt/MainActivity.java
+++ react/android/app/src/main/java/com/theabmmohi/waqt/MainActivity.java
@@ -1,12 +1,15 @@
 package com.theabmmohi.waqt;
-
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
-
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     registerPlugin(ApkDownloadManagerPlugin.class);
     super.onCreate(savedInstanceState);
+    WebSettings settings = this.bridge.getWebView().getSettings();
+    settings.setSupportZoom(false);
+    settings.setBuiltInZoomControls(false);
+    settings.setDisplayZoomControls(false);
   }
 }
