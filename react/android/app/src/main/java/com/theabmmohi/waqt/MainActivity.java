bff3ff57b0c705994f758746c4df938fa3d4f77c
theabmmohi <theabmmohi@gmail.com>
2026-08-08 10:02:48 +0000

    Fix white-flash on cold start: set proper windowBackground + WebView background instead of invalid theme attribute [Claude]

--- react/android/app/src/main/java/com/theabmmohi/waqt/MainActivity.java
+++ react/android/app/src/main/java/com/theabmmohi/waqt/MainActivity.java
@@ -1,12 +1,14 @@
 package com.theabmmohi.waqt;
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     registerPlugin(ApkDownloadManagerPlugin.class);
     super.onCreate(savedInstanceState);
+    this.bridge.getWebView().setBackgroundColor(ContextCompat.getColor(this, R.color.webview_background));
     WebSettings settings = this.bridge.getWebView().getSettings();
     settings.setSupportZoom(false);
     settings.setBuiltInZoomControls(false);
